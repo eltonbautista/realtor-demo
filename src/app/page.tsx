b077e0adc14e4a91759dfb9654ecabbe12cb3f46
EltonBautista <eltonbestaha@gmail.com>
2025-08-19 16:04:31 -0600

    Playing around with Next and Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          <li className="tracking-[-.05em]">
+            Save and see your changes instantly... Hello World
           </li>
         </ol>
 
